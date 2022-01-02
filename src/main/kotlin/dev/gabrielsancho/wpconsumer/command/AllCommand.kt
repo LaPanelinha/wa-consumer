e84efaa334225c4f3827b59dad167186ecf6b209
Gabriel Sancho <gabriel.cavalcanti@sicoob.com.br>
2022-01-02 16:44:07 -0300

    Fixed all command

--- src/main/kotlin/dev/gabrielsancho/wpconsumer/command/AllCommand.kt
+++ src/main/kotlin/dev/gabrielsancho/wpconsumer/command/AllCommand.kt
@@ -1,10 +1,12 @@
 package dev.gabrielsancho.wpconsumer.command
 
+import com.beust.jcommander.Parameter
 import dev.gabrielsancho.wpconsumer.domain.Message
 import dev.gabrielsancho.wpconsumer.extension.text
 import dev.gabrielsancho.wpconsumer.service.WhatsappService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+
 
 @Component
 class AllCommand(
@@ -14,6 +16,8 @@ class AllCommand(
     override val alias = CommandAlias.ALL_COMMAND
 
     override fun execute(message: Message) {
+        AllArguments().loadArguments(message.text)
+
         val participants = message.chat.groupMetadata?.participants?.joinToString(" ") {
             val number = it.id.replace("@c.us", "")
             return@joinToString "@$number"
@@ -22,5 +26,8 @@ class AllCommand(
         service.sendReplyWithMentions(message.chatId, participants, message.id)
     }
 
-    inner class AllArguments : CommandArguments(commandPrefix, alias)
+    inner class AllArguments : CommandArguments(commandPrefix, alias) {
+        @Parameter
+        private var parameters: List<String> = ArrayList()
+    }
 }
